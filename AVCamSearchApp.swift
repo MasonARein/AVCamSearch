7133979d28d40636a6121bfd575a5c92d5c16847
mrein6 <masonarein@gmail.com>
2025-01-23 17:34:53 -0500

    Initial commit

---
+++ AVCamSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVCamSearchApp.swift
+//  AVCamSearch
+//
+//  Created by Mason Rein on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AVCamSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
